45c4faf0a82c66a8b15db14a2bdd48f152e003f3
liuyinxin <honor@liuyinxin.com>
2019-02-20 17:26:25 +0800

    正则表达式匹配

--- 正则表达式匹配.cpp
+++ 正则表达式匹配.cpp
@@ -17,9 +17,26 @@ using namespace std;
 
 class Solution {
 public:
+// 大佬的思路,很棒,很简介
     bool match(char* str, char* pattern)
     {
-    
+        if (*str == '\0' && *pattern == '\0')
+            return true;
+        if (*str != '\0' && *pattern == '\0')
+            return false;
+        if (*(pattern + 1) != '*') {
+            if (*str == *pattern || (*str != '\0' && *pattern == '.')) {
+                return match(str + 1, pattern + 1);
+            } else {
+                return false;
+            }
+        } else {
+            if (*str == *pattern || (*str != '\0' && *pattern == '.'))
+                return match(str, pattern + 2) || match(str + 1, pattern);
+            else 
+                return match(str, pattern + 2);
+        }
+        return false;
     }
 };
 
